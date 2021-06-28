0119f726673b5cc46d51df22f8328231c8364e30
Tim Pietrusky <timpietrusky@gmail.com>
2021-06-28 13:36:18 +0200

    fix(isf-parser): Include more edge-cases

--- src/ISFParser.js
+++ src/ISFParser.js
@@ -133,16 +133,10 @@ ISFParser.prototype.replaceSpecialFunctions = function replaceSpecialFunctions(s
   source = source.replace(regex, (fullMatch, innerMatch) => `texture2D(${innerMatch}, isf_FragNormCoord)`);
 
   // IMG_PIXEL
-  regex = /IMG_PIXEL\((.+?)\s?,\s?(.+?\)?\.?.*)\)/g;
-  source = source.replace(regex, (fullMatch, sampler, coord) => {
-    return `texture2D(${sampler}, (${coord}) / RENDERSIZE)`;
-  });
+  source = this.replaceImgPixel(source);
 
   // IMG_NORM_PIXEL
-  regex = /IMG_NORM_PIXEL\((.+?)\s?,\s?(.+?\)?\.?.*)\)/g;
-  source = source.replace(regex, (fullMatch, sampler, coord) => {
-    return `VVSAMPLER_2DBYNORM(${sampler}, _${sampler}_imgRect, _${sampler}_imgSize, _${sampler}_flip, ${coord})`;
-  });
+  source = this.replaceImgNormPixel(source);
 
   // IMG_SIZE
   regex = /IMG_SIZE\((.+?)\)/g;
@@ -150,6 +144,32 @@ ISFParser.prototype.replaceSpecialFunctions = function replaceSpecialFunctions(s
     return `_${imgName}_imgSize`;
   });
   return source;
+};
+
+ISFParser.prototype.replaceImgPixel = function replaceImgPixel(source) {
+  const extractRegex = /IMG_PIXEL\((?:[^)(]+|\((?:[^)(]+|\([^)(]*\))*\))*\)/g;
+  const extractOnlySamplerCoordsRegex = /IMG_PIXEL\((.+?)\s?,\s?(.+?\)?\.?.*)\)/g;
+
+  // Find the whole string with matching parenthesis
+  return source.replace(extractRegex, (fullMatch) => {
+    // Extract sampler and coord
+    return fullMatch.replace(extractOnlySamplerCoordsRegex, (coordMatch, sampler, coord) => {
+      return `texture2D(${sampler}, (${coord}) / RENDERSIZE)`;
+    });
+  });
+};
+
+ISFParser.prototype.replaceImgNormPixel = function replaceImgNormPixel(source) {
+  const extractRegex = /IMG_NORM_PIXEL\((?:[^)(]+|\((?:[^)(]+|\([^)(]*\))*\))*\)/g;
+  const extractOnlySamplerCoordsRegex = /IMG_NORM_PIXEL\((.+?)\s?,\s?(.+?\)?\.?.*)\)/g;
+
+  // Find the whole string with matching parenthesis
+  return source.replace(extractRegex, (fullMatch) => {
+    // Extract sampler and coord
+    return fullMatch.replace(extractOnlySamplerCoordsRegex, (coordMatch, sampler, coord) => {
+      return `VVSAMPLER_2DBYNORM(${sampler}, _${sampler}_imgRect, _${sampler}_imgSize, _${sampler}_flip, ${coord})`;
+    });
+  });
 };
 
 ISFParser.prototype.buildVertexShader = function buildVertexShader() {
